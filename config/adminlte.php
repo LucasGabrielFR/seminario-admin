07f71fb5476c3d3df1ddf1ca03c5b618584ba92f
Lucas Gabriel <lucas.ddd@hotmail.com>
2024-10-23 00:15:58 -0300

    vendor public

--- config/adminlte.php
+++ config/adminlte.php
@@ -467,7 +467,7 @@ return [
             ],
         ],
         'Select2' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
--- resources/views/admin/courses/view.blade.php
+++ resources/views/admin/courses/view.blade.php
@@ -1,13 +1,11 @@
 @extends('adminlte::page')
 
-{{-- @section('plugins.Select2', true) --}}
+@section('plugins.Select2', true)
 
 @section('title', 'Ver curso')
 
 @section('content_header')
     <h1>Ver Curso</h1>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0/select2-bootstrap4.min.css" rel="stylesheet" />
 @stop
 
 @section('content')
@@ -145,8 +143,6 @@
     </div>
 @stop
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>   --}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.full.min.js"></script>
 <script>
     $(document).ready(function() {
         window.enrollStudents = function() {
