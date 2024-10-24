90c56d92a5a8d5323fd587cdd8fb7d06e1b9c2bd
Lucas Gabriel <lucas.ddd@hotmail.com>
2024-10-23 21:48:17 -0300

    Table logs

--- database/migrations/2024_10_23_214215_create_logs_table.php
+++ database/migrations/2024_10_23_214215_create_logs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('logs', function (Blueprint $table) {
-            $table->id()->autoIncrement()->primary();
+            $table->id()->autoIncrement();
             $table->string('action');
             $table->string('description');
             $table->timestamps();
